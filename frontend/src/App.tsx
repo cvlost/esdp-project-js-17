d1d8819f448de9393706f7960b94b522c1cb6d0d
Duulat Akmatbekov <duulat.akmatbekov@gmail.com>
2023-04-08 13:19:13 +0600

    #9 Удалил ненужные элементы. Подключил CSSBaseline и BrowserRouter в index.tsx. Создал constants.ts,axios.ts, types.d.ts и написал типизацию User и типизацию ошибок. Создал папки app, components, containers, создал компоненты AppToolbar.tsx, Layout.tsx, контейнер Home.tsx и подключил к App.tsx.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import { CssBaseline } from '@mui/material';
+import Home from './containers/Home';
 
 function App() {
-  return <div className="App">works</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={'Not found'} />
+        </Routes>
+      </Layout>
+    </>
+  );
 }
 
 export default App;
