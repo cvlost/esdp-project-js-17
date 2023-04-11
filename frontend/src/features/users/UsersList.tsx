905cf67b99f606506f7e8cd4ede13b2f39b6eeaf
Arslan Okenov <gtg100900@gmail.com>
2023-04-11 22:28:36 +0600

    #18 добавил защитный роут Protected.tsx и добавил его в App.tsx для того чтоб только админ мог входить в роут /users для пользователей доступны другие роуты кроме админских а для самих админов доступно всё

--- frontend/src/features/users/UsersList.tsx
+++ frontend/src/features/users/UsersList.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Box, Grid, Typography } from '@mui/material';
 import CardUser from '../../components/CardUser';
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { useAppSelector } from '../../app/hooks';
 import { selectUsersList } from './usersSlice';
 
 const UsersList = () => {
-  const dispatch = useAppDispatch();
   const usersList = useAppSelector(selectUsersList);
-  useEffect(() => {}, [dispatch]);
   return (
     <Box sx={{ py: 2 }}>
       <Typography variant="h5" component="h5">
