4b26a12c346c73ac3456c110e6509b46769f27ae
Duulat Akmatbekov <duulat.akmatbekov@gmail.com>
2023-04-15 23:43:48 +0600

    #34 Добавил модальное окно для изменения данных пользователя только для администратора

--- frontend/src/features/users/UsersList.tsx
+++ frontend/src/features/users/UsersList.tsx
@@ -1,15 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Grid, Pagination, Typography } from '@mui/material';
 import CardUser from '../../components/CardUser';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { selectUser, selectUsersListData, selectUsersListLoading, setCurrentPage } from './usersSlice';
-import { deleteUser, getUsersList } from './usersThunks';
+import {
+  selectEditingError,
+  selectEditOneUserLoading,
+  selectOneEditingUser,
+  selectUser,
+  selectUsersListData,
+  selectUsersListLoading,
+  setCurrentPage,
+} from './usersSlice';
+import { deleteUser, getEditingUser, getUsersList, logout, updateUser } from './usersThunks';
+import UserForm from '../../components/UserForm';
+import { UserMutation } from '../../types';
+import ModalBody from '../../components/ModalBody';
 
 const UsersList = () => {
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
   const usersListData = useAppSelector(selectUsersListData);
   const usersListLoading = useAppSelector(selectUsersListLoading);
+  const editingUser = useAppSelector(selectOneEditingUser);
+  const editLoading = useAppSelector(selectEditOneUserLoading);
+  const error = useAppSelector(selectEditingError);
+  const [userID, setUserID] = useState('');
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const removeUser = async (userId: string) => {
     if (user?._id !== userId) {
@@ -22,32 +39,59 @@ const UsersList = () => {
     }
   };
 
+  const openDialog = async (userId: string) => {
+    await dispatch(getEditingUser(userId));
+    setUserID(userId);
+    setIsDialogOpen(true);
+  };
+
+  const onFormSubmit = async (userToChange: UserMutation) => {
+    await dispatch(updateUser({ id: userID, user: userToChange }));
+    await dispatch(getUsersList({ page: usersListData.page, perPage: usersListData.perPage }));
+    setIsDialogOpen(false);
+    if (user && user._id === userID) {
+      await dispatch(logout());
+    }
+  };
+
   useEffect(() => {
     dispatch(getUsersList({ page: usersListData.page, perPage: usersListData.perPage }));
   }, [dispatch, usersListData.page, usersListData.perPage]);
 
   return (
-    <Box sx={{ py: 2 }}>
-      <Typography variant="h5" component="h5">
-        Список пользователей ({usersListData.count})
-      </Typography>
-      <Box>
-        <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          {usersListData.users.map((user) => (
-            <CardUser user={user} key={user._id} onDelete={() => removeUser(user._id)} />
-          ))}
-        </Grid>
-        <Pagination
-          sx={{ display: 'flex', justifyContent: 'center' }}
-          disabled={usersListLoading}
-          count={usersListData.pages}
-          page={usersListData.page}
-          onChange={(event, page) => {
-            dispatch(setCurrentPage(page));
-          }}
-        />
+    <>
+      <Box sx={{ py: 2 }}>
+        <Typography variant="h5" component="h5">
+          Список пользователей ({usersListData.count})
+        </Typography>
+        <Box>
+          <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            {usersListData.users.map((user) => (
+              <CardUser
+                user={user}
+                key={user._id}
+                onDelete={() => removeUser(user._id)}
+                onEditing={() => openDialog(user._id)}
+              />
+            ))}
+          </Grid>
+          <Pagination
+            sx={{ display: 'flex', justifyContent: 'center' }}
+            disabled={usersListLoading}
+            count={usersListData.pages}
+            page={usersListData.page}
+            onChange={(event, page) => {
+              dispatch(setCurrentPage(page));
+            }}
+          />
+        </Box>
       </Box>
-    </Box>
+      {editingUser && (
+        <ModalBody isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
+          <UserForm error={error} onSubmit={onFormSubmit} existingUser={editingUser} isEdit isLoading={editLoading} />
+        </ModalBody>
+      )}
+    </>
   );
 };
 
