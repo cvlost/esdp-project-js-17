d20edab1e735df2193d357df27d53f4cb473dce0
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-18 22:09:50 +0600

    #44 Написал логику создание города

--- backend/routers/cities.ts
+++ backend/routers/cities.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import auth from '../middleware/auth';
 import City from '../models/City';
+import permit from '../middleware/permit';
+import mongoose from 'mongoose';
 
 const citiesRouter = express.Router();
 
@@ -10,6 +12,22 @@ citiesRouter.get('/', auth, async (req, res) => {
     return res.send(cities);
   } catch {
     return res.sendStatus(500);
+  }
+});
+
+citiesRouter.post('/', auth, permit('admin'), async (req, res, next) => {
+  try {
+    const createCity = await City.create({
+      name: req.body.name,
+    });
+
+    return res.send(createCity);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e);
+    } else {
+      return next(e);
+    }
   }
 });
 
