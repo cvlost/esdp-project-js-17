92ba96bcbf5180a0fc94a037d12b8ed49bc3deb4
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-18 19:49:59 +0600

    #42 Написал логику создание региона

--- frontend/src/features/location/locationThunk.ts
+++ frontend/src/features/location/locationThunk.ts
@@ -1,8 +1,23 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { RegionList } from '../../types';
+import { GlobalError, RegionList, RegionMutation } from '../../types';
 import axiosApi from '../../axios';
+import { isAxiosError } from 'axios';
 
 export const fetchRegions = createAsyncThunk<RegionList[]>('location/fetch_regions', async () => {
   const response = await axiosApi.get<RegionList[]>('/region');
   return response.data;
 });
+
+export const createRegion = createAsyncThunk<void, RegionMutation, { rejectValue: GlobalError }>(
+  'location/create_region',
+  async (regionMutation, { rejectWithValue }) => {
+    try {
+      await axiosApi.post('/region', regionMutation);
+    } catch (e) {
+      if (isAxiosError(e) && e.response && e.response.status === 400) {
+        return rejectWithValue(e.response.data as GlobalError);
+      }
+      throw e;
+    }
+  },
+);
