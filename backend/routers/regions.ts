9379a71073c2745e6dba78608f99da30dcf599bb
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-18 19:26:43 +0600

    #42 Написал логику создание региона

--- backend/routers/regions.ts
+++ backend/routers/regions.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import auth from '../middleware/auth';
 import Region from '../models/Region';
+import permit from '../middleware/permit';
+import mongoose from 'mongoose';
 
 const regionsRouter = express.Router();
 
@@ -10,6 +12,22 @@ regionsRouter.get('/', auth, async (req, res) => {
     return res.send(regions);
   } catch {
     return res.sendStatus(500);
+  }
+});
+
+regionsRouter.post('/', auth, permit('admin'), async (req, res, next) => {
+  try {
+    const createRegion = await Region.create({
+      name: req.body.name,
+    });
+
+    return res.send(createRegion);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e);
+    } else {
+      return next(e);
+    }
   }
 });
 
