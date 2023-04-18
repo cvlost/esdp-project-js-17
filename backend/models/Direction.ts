559218d81f23a4ce9ccbbdc3109741fa01b7deeb
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-04-19 00:58:17 +0600

    #45 Создал модель направления. Одно обязательное поле name.

---
+++ backend/models/Direction.ts
@@ -0,0 +1,12 @@
+import { model, Schema } from 'mongoose';
+import { DirectionType } from '../types';
+
+const DirectionSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const Direction = model<DirectionType>('Direction', DirectionSchema);
+export default Direction;
