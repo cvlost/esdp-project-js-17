a08daeaa2efd4ee4927ec7dd908481e0945035f2
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-21 11:05:06 +0600

    #54 Описал логику получение направление в компоненте

--- frontend/src/features/location/direction/components/cardDirection.tsx
+++ frontend/src/features/location/direction/components/cardDirection.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { StyledTableRow } from '../../../../constants';
 import { IconButton, TableCell } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { DirectionType } from '../../../../types';
 
-const CardDirection = () => {
+interface Props {
+  direction: DirectionType;
+}
+
+const CardDirection: React.FC<Props> = ({ direction }) => {
   return (
     <>
       <StyledTableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-        <TableCell align="left">Test</TableCell>
+        <TableCell align="left">{direction.name}</TableCell>
         <TableCell align="right">
           <IconButton aria-label="delete">
             <DeleteIcon />
--- frontend/src/features/location/direction/createDirection.tsx
+++ frontend/src/features/location/direction/createDirection.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import { Box, Container, Paper, Table, TableBody, TableContainer, TableHead, TableRow } from '@mui/material';
+import React, { useEffect } from 'react';
+import {
+  Alert,
+  Box,
+  CircularProgress,
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 import { StyledTableCell } from '../../../constants';
 import SnackbarCard from '../../../components/SnackbarCard/SnackbarCard';
 import CardDirection from './components/cardDirection';
 import FormCreateDirection from './components/FormCreateDirection';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { selectDirections, selectDirectionsLoading } from './directionsSlice';
+import { getDirectionsList } from './directionsThunks';
 
 const CreateDirection = () => {
+  const dispatch = useAppDispatch();
+  const fetchListDirection = useAppSelector(selectDirections);
+  const fetchLoading = useAppSelector(selectDirectionsLoading);
+
+  useEffect(() => {
+    dispatch(getDirectionsList());
+  }, [dispatch]);
+
   return (
     <Box>
       <Container component="main" maxWidth="xs">
@@ -22,7 +45,23 @@ const CreateDirection = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                <CardDirection />
+                {!fetchLoading ? (
+                  fetchListDirection.length !== 0 ? (
+                    fetchListDirection.map((direct) => <CardDirection key={direct._id} direction={direct} />)
+                  ) : (
+                    <TableRow>
+                      <TableCell>
+                        <Alert severity="info">В данный момент направлений нет</Alert>
+                      </TableCell>
+                    </TableRow>
+                  )
+                ) : (
+                  <TableRow>
+                    <TableCell>
+                      <CircularProgress />
+                    </TableCell>
+                  </TableRow>
+                )}
               </TableBody>
             </Table>
           </TableContainer>
