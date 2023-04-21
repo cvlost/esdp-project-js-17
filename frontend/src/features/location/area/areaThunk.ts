5cbde1e0704f041a1b029134fa7b2253a73a8787
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-21 13:17:04 +0600

    #65 Написал логику создание в thunk

--- frontend/src/features/location/area/areaThunk.ts
+++ frontend/src/features/location/area/areaThunk.ts
@@ -1,8 +1,23 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { AreaList } from '../../../types';
+import { AreaList, AreaMutation, ValidationError } from '../../../types';
 import axiosApi from '../../../axios';
+import { isAxiosError } from 'axios';
 
 export const fetchAreas = createAsyncThunk<AreaList[]>('area/fetch_areas', async () => {
   const response = await axiosApi.get<AreaList[]>('/areas');
   return response.data;
 });
+
+export const createArea = createAsyncThunk<void, AreaMutation, { rejectValue: ValidationError }>(
+  'area/create_area',
+  async (areaMutation, { rejectWithValue }) => {
+    try {
+      await axiosApi.post('/areas', areaMutation);
+    } catch (e) {
+      if (isAxiosError(e) && e.response && e.response.status === 400) {
+        return rejectWithValue(e.response.data as ValidationError);
+      }
+      throw e;
+    }
+  },
+);
