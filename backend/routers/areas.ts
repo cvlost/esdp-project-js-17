8e796e2ac39349ebcee09a2d0bd9221d9a6b695a
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-21 13:07:01 +0600

    #65 Написал логику создание областей

--- backend/routers/areas.ts
+++ backend/routers/areas.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import auth from '../middleware/auth';
 import Area from '../models/Area';
+import permit from '../middleware/permit';
+import mongoose from 'mongoose';
 
 const areasRouter = express.Router();
 
@@ -10,6 +12,22 @@ areasRouter.get('/', auth, async (req, res) => {
     return res.send(areas);
   } catch {
     return res.sendStatus(500);
+  }
+});
+
+areasRouter.post('/', auth, permit('admin'), async (req, res, next) => {
+  try {
+    const createArea = await Area.create({
+      name: req.body.name,
+    });
+
+    return res.send(createArea);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e);
+    } else {
+      return next(e);
+    }
   }
 });
 
