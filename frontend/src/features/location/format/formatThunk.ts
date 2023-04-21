18d1dd15c5d3655bdbe032e9911500c3b5c4833d
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-21 12:55:29 +0600

    #63 Написал логику создание форматов в thunk

--- frontend/src/features/location/format/formatThunk.ts
+++ frontend/src/features/location/format/formatThunk.ts
@@ -1,8 +1,23 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { FormatList } from '../../../types';
+import { FormatList, FormatMutation, ValidationError } from '../../../types';
 import axiosApi from '../../../axios';
+import { isAxiosError } from 'axios';
 
 export const fetchFormat = createAsyncThunk<FormatList[]>('format/fetch_formats', async () => {
   const response = await axiosApi.get<FormatList[]>('/formats');
   return response.data;
 });
+
+export const createFormat = createAsyncThunk<void, FormatMutation, { rejectValue: ValidationError }>(
+  'format/create_format',
+  async (formatMutation, { rejectWithValue }) => {
+    try {
+      await axiosApi.post('/formats', formatMutation);
+    } catch (e) {
+      if (isAxiosError(e) && e.response && e.response.status === 400) {
+        return rejectWithValue(e.response.data as ValidationError);
+      }
+      throw e;
+    }
+  },
+);
