4f89751302d9ce2f7b121dbcee2a891a0cbe439a
Polushin Dima <dima2004polushin2004@gmail.com>
2023-04-21 12:45:34 +0600

    #63 Написал логику создание форматов

--- backend/routers/formats.ts
+++ backend/routers/formats.ts
@@ -3,6 +3,7 @@ import auth from '../middleware/auth';
 import { FormatType } from '../types';
 import permit from '../middleware/permit';
 import Format from '../models/Format';
+import mongoose from 'mongoose';
 
 const formatRouter = express.Router();
 
@@ -11,6 +12,21 @@ formatRouter.get('/', auth, permit('admin'), async (req, res, next) => {
     const formats: FormatType[] = await Format.find();
     return res.send(formats);
   } catch (e) {
+    return next(e);
+  }
+});
+
+formatRouter.post('/', auth, permit('admin'), async (req, res, next) => {
+  try {
+    const newFormat = await Format.create({
+      name: req.body.name,
+    });
+
+    return res.send(newFormat);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e);
+    }
     return next(e);
   }
 });
