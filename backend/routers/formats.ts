b1fc823bb4014c4730744cd89327e4c3e9a0e80e
Duulat Akmatbekov <duulat.akmatbekov@gmail.com>
2023-04-22 18:28:11 +0600

    #64 Создал роутер streets.ts, добавил роутер в index.ts. Модифицировал все роутеры к одному формату кода (добавил next, проверку на наличие, поменял название при создании на Data, убрал лишнюю типизацию).

--- backend/routers/formats.ts
+++ backend/routers/formats.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import auth from '../middleware/auth';
-import { FormatType } from '../types';
 import permit from '../middleware/permit';
 import Format from '../models/Format';
 import mongoose from 'mongoose';
@@ -9,7 +8,7 @@ const formatRouter = express.Router();
 
 formatRouter.get('/', auth, permit('admin'), async (req, res, next) => {
   try {
-    const formats: FormatType[] = await Format.find();
+    const formats = await Format.find();
     return res.send(formats);
   } catch (e) {
     return next(e);
@@ -18,11 +17,11 @@ formatRouter.get('/', auth, permit('admin'), async (req, res, next) => {
 
 formatRouter.post('/', auth, permit('admin'), async (req, res, next) => {
   try {
-    const newFormat = await Format.create({
+    const formatData = await Format.create({
       name: req.body.name,
     });
 
-    return res.send(newFormat);
+    return res.send(formatData);
   } catch (e) {
     if (e instanceof mongoose.Error.ValidationError) {
       return res.status(400).send(e);
@@ -33,9 +32,14 @@ formatRouter.post('/', auth, permit('admin'), async (req, res, next) => {
 
 formatRouter.delete('/:id', auth, permit('admin'), async (req, res, next) => {
   try {
-    const { id } = req.params;
-    await Format.deleteOne({ _id: id });
-    return res.send({ remove: id });
+    const { _id } = req.params;
+    const format = await Format.findById(_id);
+    if (!format) {
+      return res.status(404).send({ error: 'Формат не существует в базе.' });
+    }
+
+    const result = await Format.deleteOne({ _id });
+    return res.send(result);
   } catch (e) {
     return next(e);
   }
