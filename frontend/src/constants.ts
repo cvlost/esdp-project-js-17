3000e9c9c46c5ea8cd2a45935927bb3c2c92f4d4
Duulat Akmatbekov <duulat.akmatbekov@gmail.com>
2023-04-28 23:11:14 +0600

    #56 Добавил константу размеров

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -33,3 +33,5 @@ export const StyledTableRow = styled(TableRow)(() => ({
     border: 0,
   },
 }));
+
+export const BILLBOARD_SIZES = ['2,7x5,7', '2x5', '3x12', '3x14,42', '3x16', '3x18', '3x6', '4x10'] as const;
