859b21c11d6392ca81637946015a8647060a823c
Arslan Okenov <gtg100900@gmail.com>
2023-04-29 23:44:58 +0600

    #88 убрал обязательную авторизацию в роуте locations.ts и убрал typography в home tsx

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Typography } from '@mui/material';
 import HomeList from '../components/Home/HomeList';
 
 const Home = () => {
   return (
     <>
-      <Typography></Typography>
       <HomeList />
     </>
   );
