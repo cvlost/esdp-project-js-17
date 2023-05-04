8745142b27a812ca512b266a71d87c95e95cfef9
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-05-04 23:37:44 +0600

    #109 Создал модель Client

---
+++ backend/models/Client.ts
@@ -0,0 +1,22 @@
+import { model, Schema } from 'mongoose';
+import { ClientType } from '../types';
+
+const ClientSchema = new Schema<ClientType>({
+  name: {
+    type: 'String',
+    required: true,
+  },
+  phone: {
+    type: 'String',
+    required: true,
+  },
+  email: {
+    type: 'String',
+  },
+  description: {
+    type: 'String',
+  },
+});
+
+const Client = model('Client', ClientSchema);
+export default Client;
