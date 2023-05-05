50d0313961d0e00e86b1de85dca72fe5e7a5dd01
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-05-05 22:52:29 +0600

    #109 Добавил интерфейс Брони и изменил интерфейс Локации, подключил схему клиента и брони также близжайшая дата брони.

--- backend/types.d.ts
+++ backend/types.d.ts
@@ -38,7 +38,7 @@ export interface IPeriod {
 export interface ILocation {
   client: Schema.Types.ObjectId;
   booking: Schema.Types.ObjectId;
-  nearest_booking_date: Schema.Types.Date[];
+  nearest_booking_date: [Schema.Types.Date];
   country: string;
   area: Types.ObjectId;
   region: Types.ObjectId;
@@ -76,5 +76,5 @@ export interface ClientType {
 export interface BookingType {
   clientId: Schema.Types.ObjectId;
   locationId: Schema.Types.ObjectId;
-  booking_date: Schema.Types.Date[];
+  booking_date: [Schema.Types.Date];
 }
