1dd96340b1aed51bcffc61d984fc7e7069fde561
Polushin Dima <dima2004polushin2004@gmail.com>
2023-05-18 20:32:43 +0600

    #126 Написал логику получение локаций в сгенерированной ссылки

--- backend/routers/commercialLinks.ts
+++ backend/routers/commercialLinks.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import auth from '../middleware/auth';
 import CommercialLink from '../models/CommercialLink';
+import Location from '../models/Location';
+import { CommercialLinkType } from '../types';
+import * as crypto from 'crypto';
 
 const commercialLinksRouter = express.Router();
 
-commercialLinksRouter.get('/:shortUrl', auth, async (req, res) => {
+commercialLinksRouter.get('/:shortUrl', async (req, res) => {
   try {
     const commLink = await CommercialLink.findOne({ shortUrl: req.params.shortUrl });
 
@@ -17,7 +19,7 @@ commercialLinksRouter.get('/:shortUrl', auth, async (req, res) => {
   }
 });
 
-commercialLinksRouter.post('/', auth, async (req, res) => {
+commercialLinksRouter.post('/', async (req, res) => {
   const randomShortUrl = crypto.randomUUID();
   try {
     const newCommLink = await CommercialLink.create({
@@ -33,6 +35,40 @@ commercialLinksRouter.post('/', auth, async (req, res) => {
   } catch (e) {
     return res.sendStatus(500);
   }
+});
+
+commercialLinksRouter.get('/location/:id', async (req, res) => {
+  const commLink: CommercialLinkType | null = await CommercialLink.findOne({ _id: req.params.id });
+
+  if (!commLink) return res.status(500).send({ error: 'Ссылка недействительна !' });
+  const selects: { [key: string]: number } = {};
+
+  commLink.settings.forEach((item) => {
+    selects[item.name] = 1;
+  });
+
+  const locations = await Location.find({ _id: commLink.location });
+  const fullLocations = await Location.aggregate([
+    { $lookup: { from: 'cities', localField: 'city', foreignField: '_id', as: 'city' } },
+    { $lookup: { from: 'regions', localField: 'region', foreignField: '_id', as: 'region' } },
+    { $lookup: { from: 'streets', localField: 'street', foreignField: '_id', as: 'street' } },
+    { $lookup: { from: 'areas', localField: 'area', foreignField: '_id', as: 'area' } },
+    { $lookup: { from: 'formats', localField: 'format', foreignField: '_id', as: 'format' } },
+    { $lookup: { from: 'directions', localField: 'direction', foreignField: '_id', as: 'direction' } },
+    { $lookup: { from: 'legalentities', localField: 'legalEntity', foreignField: '_id', as: 'legalEntity' } },
+    { $set: { city: { $first: '$city.name' } } },
+    { $set: { region: { $first: '$region.name' } } },
+    { $set: { street: { $first: '$street.name' } } },
+    { $set: { direction: { $first: '$direction.name' } } },
+    { $set: { area: { $first: '$area.name' } } },
+    { $set: { format: { $first: '$format.name' } } },
+    { $set: { legalEntity: { $first: '$legalEntity.name' } } },
+    { $set: { price: { $convert: { input: '$price', to: 'string' } } } },
+    { $match: { _id: { $in: locations.map((loc) => loc._id) } } },
+    { $project: selects },
+  ]);
+
+  return res.send(fullLocations);
 });
 
 export default commercialLinksRouter;
