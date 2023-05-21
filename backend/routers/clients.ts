b592b302a5d2e3c942b24bf6dffc5ee9b28c3edc
Arslan Okenov <gtg100900@gmail.com>
2023-05-22 00:22:31 +0600

    #127 изменил get  запрос

--- backend/routers/clients.ts
+++ backend/routers/clients.ts
@@ -22,22 +22,9 @@ clientsRouter.post('/', auth, async (req, res, next) => {
 });
 
 clientsRouter.get('/', auth, async (req, res, next) => {
-  let page = parseInt(req.query.page as string);
-  let perPage = parseInt(req.query.perPage as string);
-
-  page = isNaN(page) || page <= 0 ? 1 : page;
-  perPage = isNaN(perPage) || perPage <= 0 ? 10 : perPage;
-
   try {
-    const count = await Client.count();
-    let pages = Math.ceil(count / perPage);
-    if (pages === 0) pages = 1;
-    if (page > pages) page = pages;
-    const clients = await Client.find()
-      .skip((page - 1) * perPage)
-      .limit(perPage);
-
-    return res.send({ clients, page, pages, count, perPage });
+    const clients = await Client.find();
+    return res.send(clients);
   } catch (e) {
     return next(e);
   }
