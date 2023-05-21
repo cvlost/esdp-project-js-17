d976b5278a307b016155b169d08ccfa574af93cb
Arslan Okenov <gtg100900@gmail.com>
2023-05-21 21:01:49 +0600

    #127 добавил put запрос

--- backend/routers/clients.ts
+++ backend/routers/clients.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import auth from '../middleware/auth';
 import Client from '../models/Client';
 import mongoose from 'mongoose';
+import User from '../models/Users';
+import usersRouter from './users';
 
 const clientsRouter = express.Router();
 
@@ -49,6 +51,37 @@ clientsRouter.get('/:id', auth, async (req, res, next) => {
     return res.send(client);
   } catch (e) {
     return next(e);
+  }
+});
+
+clientsRouter.put('/:id', auth, async (req, res, next) => {
+  try {
+    const id = req.params.id as string;
+    const { email, name, phone } = req.body;
+    const client = await Client.findById(id);
+
+    if (!client) {
+      return res.status(404).send({ error: 'client not found!' });
+    }
+
+    if (email && email !== client.email) {
+      client.email = email;
+    }
+    if (name && name !== client.name) {
+      client.name = name;
+    }
+    if (phone && phone !== client.phone) {
+      client.phone = phone;
+    }
+
+    const result = await client.save();
+
+    return res.send(result);
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(error);
+    }
+    return next(error);
   }
 });
 
