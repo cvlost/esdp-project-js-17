204578839af0055896547c906b6bf125f1f79531
Polushin Dima <dima2004polushin2004@gmail.com>
2023-05-27 19:17:03 +0600

    #147 Написал логику удаления в backend

--- backend/routers/bookings.ts
+++ backend/routers/bookings.ts
@@ -25,4 +25,24 @@ bookingsRouter.post('/', auth, async (req, res, next) => {
   }
 });
 
+bookingsRouter.delete('/:loc/:book', auth, async (req, res) => {
+  const locId = req.params.loc as string;
+  const bookId = req.params.book as string;
+
+  try {
+    const locationOne = await Location.findOne({ _id: locId });
+
+    if (!locationOne) {
+      return res.status(404).send({ error: 'Данной локации нет ' });
+    }
+
+    await Booking.deleteOne({ _id: bookId });
+    await Location.updateOne({ _id: locId }, { $pull: { booking: bookId } });
+
+    return res.send({ removeLoc: locId, removeBook: bookId });
+  } catch (e) {
+    return res.sendStatus(500);
+  }
+});
+
 export default bookingsRouter;
