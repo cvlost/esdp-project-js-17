aa10fa82c7e3df07e41d43c04824170132e23a28
Arslan Okenov <gtg100900@gmail.com>
2023-05-28 17:20:14 +0600

    #128 изменил и доработал бэк редактирования клиента

--- backend/routers/clients.ts
+++ backend/routers/clients.ts
@@ -52,79 +52,31 @@ clientsRouter.get('/:id', auth, permit('admin'), async (req, res, next) => {
 });
 
 clientsRouter.put('/:id', auth, permit('admin'), async (req, res, next) => {
+  const clientEdit = {
+    companyName: req.body.companyName,
+    companyKindOfActivity: req.body.companyKindOfActivity,
+    companyAddress: req.body.companyAddress,
+    companyPhone: req.body.companyPhone,
+    companyEmail: req.body.companyEmail,
+    companySite: req.body.companySite,
+    companyBirthday: req.body.companyBirthday,
+    CompanyManagementName: req.body.CompanyManagementName,
+    CompanyManagementJobTitle: req.body.CompanyManagementJobTitle,
+    CompanyManagementBirthday: req.body.CompanyManagementBirthday,
+    contactPersonName: req.body.contactPersonName,
+    contactPersonJobTitle: req.body.contactPersonJobTitle,
+    contactPersonBirthday: req.body.contactPersonBirthday,
+    advertisingChannel: req.body.advertisingChannel,
+  };
   try {
     const id = req.params.id as string;
-    const {
-      companyName,
-      companyKindOfActivity,
-      companyAddress,
-      companyPhone,
-      companyEmail,
-      companySite,
-      companyBirthday,
-      CompanyManagementName,
-      CompanyManagementJobTitle,
-      CompanyManagementBirthday,
-      contactPersonName,
-      contactPersonJobTitle,
-      contactPersonBirthday,
-      advertisingChannel,
-    } = req.body;
     const client = await Client.findById(id);
-
     if (!client) {
       return res.status(404).send({ error: 'client not found!' });
     }
+    await Client.updateMany({ _id: id }, clientEdit);
 
-    if (companyEmail && companyEmail !== client.companyEmail) {
-      client.companyEmail = companyEmail;
-    }
-    if (companyName && companyName !== client.companyName) {
-      client.companyName = companyName;
-    }
-    if (companyKindOfActivity && companyKindOfActivity !== client.companyKindOfActivity) {
-      client.companyKindOfActivity = companyKindOfActivity;
-    }
-    if (companyAddress && companyAddress !== client.companyAddress) {
-      client.companyAddress = companyAddress;
-    }
-    if (companyPhone && companyPhone !== client.companyPhone) {
-      client.companyPhone = companyPhone;
-    }
-    if (companySite && companySite !== client.companySite) {
-      client.companySite = companySite;
-    }
-    if (companySite && companySite !== client.companySite) {
-      client.companySite = companySite;
-    }
-    if (companyBirthday && companyBirthday !== client.companyBirthday) {
-      client.companyBirthday = companyBirthday;
-    }
-    if (CompanyManagementName && CompanyManagementName !== client.CompanyManagementName) {
-      client.CompanyManagementName = CompanyManagementName;
-    }
-    if (CompanyManagementJobTitle && CompanyManagementJobTitle !== client.CompanyManagementJobTitle) {
-      client.CompanyManagementJobTitle = CompanyManagementJobTitle;
-    }
-    if (CompanyManagementBirthday && CompanyManagementBirthday !== client.CompanyManagementBirthday) {
-      client.CompanyManagementBirthday = CompanyManagementBirthday;
-    }
-    if (contactPersonName && contactPersonName !== client.contactPersonName) {
-      client.contactPersonName = contactPersonName;
-    }
-    if (contactPersonJobTitle && contactPersonJobTitle !== client.contactPersonJobTitle) {
-      client.contactPersonJobTitle = contactPersonJobTitle;
-    }
-    if (contactPersonBirthday && contactPersonBirthday !== client.contactPersonBirthday) {
-      client.contactPersonBirthday = contactPersonBirthday;
-    }
-    if (advertisingChannel && advertisingChannel !== client.advertisingChannel) {
-      client.advertisingChannel = advertisingChannel;
-    }
-
-    const result = await client.save();
-
-    return res.send(result);
+    return res.send('Edited: ' + id);
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
       return res.status(400).send(error);
