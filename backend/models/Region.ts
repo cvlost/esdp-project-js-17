8ad02eb63baec786f38e72b4ef77dc15e01f83c8
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-05-30 23:59:56 +0600

    #124 FIX. Решение конфлитов с регионами.

--- backend/models/Region.ts
+++ backend/models/Region.ts
@@ -1,4 +1,4 @@
-import { HydratedDocument, model, Schema, Types } from 'mongoose';
+import { model, Schema, Types } from 'mongoose';
 import { RegionType } from '../types';
 import City from './City';
 
@@ -6,15 +6,6 @@ const RegionSchema = new Schema<RegionType>({
   name: {
     type: String,
     required: true,
-    unique: true,
-    validate: {
-      validator: async function (this: HydratedDocument<RegionType>, name: string): Promise<boolean> {
-        if (!this.isModified('name')) return true;
-        const region: HydratedDocument<RegionType> | null = await Region.findOne({ name });
-        return !region;
-      },
-      message: 'Такой район уже существуеют!',
-    },
   },
   city: {
     type: Schema.Types.ObjectId,
